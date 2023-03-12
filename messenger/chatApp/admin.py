94ff795899b741c58e1cfacb96ea9329185067ab
Akbar <abdullaev.akbar@gmail.com>
2023-03-13 00:38:34 +0500

    .

--- messenger/chatApp/admin.py
+++ messenger/chatApp/admin.py
@@ -2,9 +2,8 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Rooms, Messages, Profile, Members
+from .models import Rooms, Messages, Profile
 
 admin.site.register(Rooms)
 admin.site.register(Messages)
 admin.site.register(Profile)
-admin.site.register(Members)
